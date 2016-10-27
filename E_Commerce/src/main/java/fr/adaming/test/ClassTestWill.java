f4d572a331a219bf9c2335e3d5075ab733e501c3
inti0226 <inti0226@INTI-0226.home>
2016-10-27 14:31:06 +0200

    Willem: just commit and p

--- E_Commerce/src/main/java/fr/adaming/test/ClassTestWill.java
+++ E_Commerce/src/main/java/fr/adaming/test/ClassTestWill.java
@@ -1,8 +1,24 @@
 package fr.adaming.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import fr.adaming.service.CategorieServiceImpl;
+import fr.adaming.service.ICategorieService;
+
 public class ClassTestWill {
 
 	public static void main(String[] args) {
+
+		String context="C:\\Users\\inti0226\\git\\E_commerce\\E_Commerce\\src\\main\\webapp\\WEB-INF\\application-context.xml";
+		
+		ApplicationContext cxt=new FileSystemXmlApplicationContext(context);
+		
+		ICategorieService catServ=new CategorieServiceImpl();
+		
+		
+		
 		
 	}
 }
