8bbbe52c84f66fafea77d667194311a2c10f1ca9
inti0293 <inti0293@DESKTOP-4C50ANM.home>
2016-10-27 20:22:13 +0200

    FABIEN : Bootstrap ok !!

---
+++ E_Commerce/src/main/java/fr/adaming/controller/ClientController.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ * @author CLAIN Fabien
+ * Date :27/10/2016
+ * 
+ */
+package fr.adaming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/index")
+public class ClientController {
+//----------------------------------------------------------------------------------------------------------------
+//---------------------------------1_Les propriétés (champs, attributs)-------------------------------------------
+	/**
+	 * 1_Les propriétés (champs, attributs)
+	 */
+//----------------------------------------------------------------------------------------------------------------
+//---------------------------------2_Les constructeurs------------------------------------------------------------	
+	/**
+	 * 2_Les constructeurs
+	 */
+//----------------------------------------------------------------------------------------------------------------
+//---------------------------------3_Les Getters et Setters-------------------------------------------------------
+	/**
+	 * 3_Les Getters et Setters
+	 */
+//----------------------------------------------------------------------------------------------------------------
+//---------------------------------4_Méthodes---------------------------------------------------------------------
+	/**
+	 * 4_Méthodes
+	 */
+	@RequestMapping(value="/accueil", method=RequestMethod.GET)
+	public String welcomeEmploye(ModelMap model){
+
+		return "c_accueil";
+	}
+//----------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------
+}
---
+++ E_Commerce/src/main/webapp/WEB-INF/pages/c_accueil.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+
+<link href="<c:url value="/resources/styles/bootstrap.min.css" />" rel="stylesheet">
+<script src="<c:url value="/resources/javascript/bootstrap.min.js" />"></script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+
+<title>Insert title here</title>
+</head>
+<body>
+
+<a href="#" class="btn btn-primary btn-lg disabled" role="button">Primary link</a>
+<a href="#" class="btn btn-default btn-lg disabled" role="button">Link</a>
+
+<script src="webjars/jquery/1.11.1/jquery.min.js"></script>
+<script src="webjars/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</body>
+</html>
